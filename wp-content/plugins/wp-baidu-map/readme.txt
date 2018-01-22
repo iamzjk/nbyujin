b75457bf47cb02e1f7d5a2fe0ae243e4aa32cf99
kun <iamzjk@gmail.com>
2018-01-22 11:35:44 +0800

    1000 to 10000; added email link

--- wp-content/plugins/wp-baidu-map/readme.txt
+++ wp-content/plugins/wp-baidu-map/readme.txt
@@ -1,17 +1,17 @@
 ﻿=== WP Baidu Map ===
 Contributors: suifengtec
-Donate link: https://coolwp.com/wp-baidu-map.html
+Donate link: http://coolwp.com/wp-baidu-map.html
 Tags: map, baidu
 Requires at least: 3.0.1
-Tested up to: 4.7.4
-Stable tag: 1.1
+Tested up to: 4.9.1
+Stable tag: 1.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 这是一个用于在你的网站上显示自定义百度地图的插件。
 == Description ==
 
-真搞不明白为什么那么多人把针对百度搜索引擎的网站地图插件叫做WordPress百度地图插件,那么叫的话,让真正的WordPress百度地图插件情何以堪啊?
+真搞不明白为什么那么多人把针对百度搜索引擎的网站地图插件叫做 WordPress 百度地图插件,那么叫的话,让真正的WordPress百度地图插件情何以堪啊?
 
 
 = 使用 =
@@ -23,8 +23,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 或者，你也可使用短代码自行插入地图: 在你想调用百度地图的地方使用短代码调用百度地图，示例:
 
 `
-[baidu_map id='abcd' w='100%' h='450px' lon='135' lat='35' biz_name='索凌网络' address='这里输入地址' email='a@suoling.net' phone='010-12345678']
+[baidu_map id='abcd' zl='17' nm='0' w='100%' h='450px' lon='135' lat='35' biz_name='酷威普' address='这里输入地址' email='my@email.ltd' phone='010-12345678']
 `
+
+支持自定义地图的缩放等级(默认为17)，支持隐藏地图上跳动的 marker.
 
 == Screenshots ==
 
@@ -37,8 +39,23 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 = 安装 =
 就像其它插件那样安装就行了！
 
+安装之后,需要更换主文件 wp-baidu-map.php 中的百度地图AK：
+`
+/*================= 设置百度地图 AK =================*/
+    public static $ak = '你的百度地图AK';
+
+/*================= //设置百度地图 AK =================*/
+`
+
+百度地图AK，可以在百度地图开发者平台免费申请到。
+
+
 = 参数说明 =
-id : 必需，以英文开头的英文字母、数字和中横线的组合；
+id : 必需，默认为 map_<当前的时间戳>以英文开头的英文字母、数字和中横线的组合；
+
+zl: 可选, 默认为 17, 缩放等级,1-18 闭区间内的整数;
+
+nm: 可选,默认为 0, 是否隐藏地图上跳动的 marker;
 
 w : 可选,字符串,默认为 `100%`;
 
@@ -55,15 +72,18 @@ address : 可选的字符串，指企业/团队的地址 ；
 email： 可选的字符串，指企业/团队的电邮地址；
 
 phone : 可选的整型数，指企业/团队的电话号码 。
+
 == Frequently Asked Questions ==
 
 应该不会有什么使用上的困难的，因为很好用。
-如果有的话，请移步至[WP Baidu Map的主页](http://suoling.net/wp-baidu-map/)获取帮助
-
+如果有的话，请移步至[WP Baidu Map的主页](http://coolwp.com/wp-baidu-map.html)获取帮助
 
 
 == Changelog ==
-
+= 1.2.1 =
+修复一处非关键性的语法错误;
+= 1.2 =
+结合用户邮件反馈的建议,增加两个选项(一个用于控制缩放等级,默认的缩放等级为17,一个用于控制是否显示地图上跳动的 marker);
 = 1.1 =
 支持编辑器快捷输入;
 在快捷输入中支持将地址转换为百度地图的经纬度;
@@ -75,6 +95,10 @@ phone : 可选的整型数，指企业/团队的电话号码 。
 
 
 == Upgrade Notice ==
+= 1.2.1 =
+直接升级即可;
+= 1.2=
+CSS Class "mywindow" 改为"bdmpwindow";
 = 1.1 =
 直接升级即可;
 = 1.0 =
--- wp-content/plugins/wp-baidu-map/wp-baidu-map.php
+++ wp-content/plugins/wp-baidu-map/wp-baidu-map.php
@@ -1,29 +1,33 @@
 <?php
 /*
 Plugin Name:WP Baidu Map
-Plugin URI: https://coolwp.com/wp-baidu-map.html
+Plugin URI: http://coolwp.com/wp-baidu-map.html
 Description: A real Baidu map plugin.
 Author:suifengtec
-Version: 1.1
+Version: 1.2.1
 Author URI: https://coolwp.com
 */
 /*
 1.0 : 初始发布;
 1.1 : 添加编辑器按钮插入;支持将输入的地址转换为百度地图的坐标;代码优化;支持在同一页面显示N多地图;
+1.2 : 增加两个选项(一个用于控制缩放等级,默认的缩放等级为17,一个用于控制是否显示地图上跳动的 marker);
+1.2.1 : 修复一处非关键性的语法错误;
  */
 
 defined('ABSPATH') || exit;
-
 
 if(!class_exists('WP_Baidu_Maps')):
 
 class WP_Baidu_Maps{
 
     /*百度地图AK , 请在 http://lbsyun.baidu.com/apiconsole/key 这里申请,免费的。*/
-
+/*================= 设置百度地图 AK =================*/
     public static $ak = '5upeZi9ESotIhhBvDkERXuUB';
 
+/*================= //设置百度地图 AK =================*/
+
     private static $instance;
+    private static $ver = '1.2';
     public function __wakeup() {}
     public function __clone() {}
     public function __construct(){ }
@@ -36,7 +40,7 @@ class WP_Baidu_Maps{
         return self::$instance;
 
     }
-    
+
     public function hooks(){
         add_action('init', array( $this, 'add_shortcode' ) );
         add_action('admin_init', array ( __CLASS__, 'add_insert_btn' ) );
@@ -63,7 +67,6 @@ class WP_Baidu_Maps{
 
         echo '<script>var baiduMapAK ="'.self::$ak.'";</script>';
 
-
         $plugin_array['baidumap'] =  plugins_url('js/baidu-map.js',__FILE__);
         return $plugin_array;
     }
@@ -72,13 +75,15 @@ class WP_Baidu_Maps{
 
     public  function add_shortcode(){
 
-        add_shortcode('baidu_map', array( __CLASS__ ,'add_sc' ) );
+        add_shortcode('baidu_map', array( $this ,'add_sc' ) );
     }
 
     public  function add_sc( $atts= array(), $content='' ){
 
         extract(shortcode_atts(array(
             'id'=>'',
+            'zl'=>17,
+            'nm'=>0,
             'w'=>'',
             'h'=>'',
             'lon'=>'',
@@ -91,61 +96,34 @@ class WP_Baidu_Maps{
             ) ,$atts));
 
         $id = $id?$id:'wp-baidu-map';
+        $nm = isset($nm)?$nm:0;
+         $zl = $zl?$zl:17;
         $w = $w?$w:'100%';
         $h = $h?$h:'400px';
-        $biz_name = $biz_name?$biz_name:false;
         $lon = $lon?$lon:false;
         $lat = $lat?$lat:false;
-        $address = $address?$address:false;
-        $email = $email?$email:false;
-        $phone = $phone?$phone:false;
+
 
         if(!$lon||!$lat){
             return '设置经纬度后才能显示地图哦！';
         }
 
         ob_start();
-        ?>
-        <script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=<?php echo $ak?>"></script>
-        <style>
-            div#<?php echo esc_attr($id) ?> {display: block !important;}
-            #<?php echo esc_attr($id) ?> {width:<?php echo $w ?>; height:<?php echo $h ?>;overflow: hidden;margin:0;}
-            #l-map{height:100%;width:78%;float:left;border-right:2px solid #bcbcbc;}
-            #r-result{height:100%;width:20%;float:left;}
-            .mywindow{ height:auto; width:auto; font-size:12px; line-height:22px;}
-            .mylocationcontainer{width:100%; height:100%; margin:0 auto;}
-            .mapimg{width:100%;height:100%;}
-           .BMap_cpyCtrl span,.anchorBL{display:none!important;}
-        </style>
+        ?><script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=<?php echo $ak?>"></script>
+        <style>.map{display:block;margin-bottom:1em}div#<?php echo esc_attr($id) ?> {display: block !important;}
+            #<?php echo esc_attr($id) ?> {width:<?php echo $w ?>; height:<?php echo $h ?>;overflow: hidden;margin:0}.bdmapwindow{ height:auto; width:auto; font-size:12px; line-height:22px}.BMap_cpyCtrl span,.anchorBL{display:none!important} </style>
         <div class="map"><div id="<?php echo esc_attr($id) ?>"></div></div>
-        <script type="text/javascript">
-            var map<?php echo esc_attr($id) ?> = new BMap.Map("<?php echo esc_attr($id) ?>");
-            var point = new BMap.Point(<?php echo $lon ?>,<?php echo $lat ?>);
-            map<?php echo esc_attr($id) ?>.enableScrollWheelZoom(true);
-            map<?php echo esc_attr($id) ?>.enableContinuousZoom();
-            map<?php echo esc_attr($id) ?>.addControl(new BMap.NavigationControl());
-            var marker = new BMap.Marker(point);
-            marker.setAnimation(BMAP_ANIMATION_BOUNCE);
-            var sContent ='<div class="mywindow"><?php echo $biz_name ?><?php if(!empty($address)){echo '<br>地址：'.$address;} ?><?php if(!empty($phone)){echo '<br>电话：'.$phone;} ?><?php if(!empty($email)){echo '<br>电邮：'.$email;} ?></div>';
-
-            var infoWindow = new BMap.InfoWindow(sContent);
-            map<?php echo esc_attr($id) ?>.centerAndZoom(point, 9);
-            map<?php echo esc_attr($id) ?>.addOverlay(marker);
-            marker.addEventListener("click", function(){
-               this.openInfoWindow(infoWindow);
-               window.onload = function (){
-                   infoWindow.redraw();
-               }
-            });
-        </script>
+        <script type="text/javascript">;var map<?php echo esc_attr($id) ?> = new BMap.Map("<?php echo esc_attr($id) ?>");var point = new BMap.Point(<?php echo $lon ?>,<?php echo $lat ?>);map<?php echo esc_attr($id) ?>.enableScrollWheelZoom(true);map<?php echo esc_attr($id) ?>.enableContinuousZoom(true); map<?php echo esc_attr($id) ?>.addControl(new BMap.NavigationControl());map<?php echo esc_attr($id) ?>.centerAndZoom(point,<?php echo $zl; ?>);
+            <?php if(!$nm&&($address||$email||$phone||$biz_name )): ?>var marker = new BMap.Marker(point); marker.setAnimation(BMAP_ANIMATION_BOUNCE); map<?php echo esc_attr($id) ?>.addOverlay(marker);var sContent ='<div class="bdmapwindow"><?php echo $biz_name ?><?php if(!empty($address)){echo '<br>地址：'.$address;} ?><?php if(!empty($phone)){echo '<br>电话：'.$phone;} ?><?php if(!empty($email)){echo '<br>电邮：'.$email;} ?></div>'; var infoWindow = new BMap.InfoWindow(sContent); marker.addEventListener("click", function(){this.openInfoWindow(infoWindow);window.onload = function (){infoWindow.redraw() }});
+            <?php endif; ?></script>
        <?php
-
-        $output = ob_get_clean();
-        return $output;
+        $o = ob_get_contents();
+        ob_end_clean();
+        return $o;
 
 
     }
-    
+
 }
 
 $GLOBALS['wp_baidu_map'] = WP_Baidu_Maps::instance();
